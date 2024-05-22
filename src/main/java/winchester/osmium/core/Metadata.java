312dccd433dc9b30f8b1f4590129822426ddae36
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-22 12:13:14 +0100

    Add singleton implementation

--- src/main/java/winchester/osmium/core/Metadata.java
+++ src/main/java/winchester/osmium/core/Metadata.java
@@ -1,4 +1,15 @@
 package winchester.osmium.core;
 
 public class Metadata {
+
+    public static Metadata getInstance() {
+        if (instance == null) {
+            instance = new Metadata();
+        }
+        return instance;
+    }
+
+    private static Metadata instance;
+
+    private Metadata() { }
 }
