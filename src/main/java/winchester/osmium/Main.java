150a70fd58393f3bb821d3bccf2f8fa1949068a0
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-22 12:14:10 +0100

    Invoke init and start functions

--- src/main/java/winchester/osmium/Main.java
+++ src/main/java/winchester/osmium/Main.java
@@ -1,9 +1,12 @@
 package winchester.osmium;
 
+import winchester.osmium.core.Program;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Program.initialise();
+        Program.start();
     }
 
 }
