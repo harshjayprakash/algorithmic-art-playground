ea0c8c8a287028d1043b7fc64a4944b97fb4bae8
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-22 12:12:16 +0100

    Add initialised property

--- src/main/java/winchester/osmium/core/Program.java
+++ src/main/java/winchester/osmium/core/Program.java
@@ -1,5 +1,9 @@
 package winchester.osmium.core;
 
 public abstract class Program {
+    private static boolean initialised = false;
 
+    public static boolean isInitialised() {
+        return initialised;
+    }
 }
