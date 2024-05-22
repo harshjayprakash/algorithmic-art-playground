9530ebee58d94366f63792cf01ad94b3ea1df3b3
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-22 12:12:32 +0100

    Add init static function

--- src/main/java/winchester/osmium/core/Program.java
+++ src/main/java/winchester/osmium/core/Program.java
@@ -3,6 +3,15 @@ package winchester.osmium.core;
 public abstract class Program {
     private static boolean initialised = false;
 
+    public static void initialise() {
+        if (initialised) { return; }
+
+        Metadata.getInstance().setAppTitle("Generative Art Playground");
+        Metadata.getInstance().setAppVersion("1.0");
+
+        initialised = true;
+    }
+
     public static boolean isInitialised() {
         return initialised;
     }
