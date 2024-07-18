67a390fc76467af0049e573e2e7986fe42921a76
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-07-18 12:35:35 +0100

    Updated help documentation

--- src/main/java/winchester/osmium/presentation/windows/HelpFrame.java
+++ src/main/java/winchester/osmium/presentation/windows/HelpFrame.java
@@ -4,13 +4,13 @@ import javax.swing.*;
 
 public class HelpFrame extends JFrame {
     private JScrollPane scrollPane;
-    private JTextArea textArea;
+    private JEditorPane textArea;
 
     public HelpFrame() {
         super();
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setSize(800, 600);
-        this.setTitle("Functions List");
+        this.setTitle("Help");
 
         this.initialiseComponents();
         this.initialiseHandlers();
@@ -21,36 +21,58 @@ public class HelpFrame extends JFrame {
         this.scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         this.scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-        this.textArea = new JTextArea();
+        this.textArea = new JEditorPane();
         this.textArea.setEditable(false);
+        this.textArea.setContentType("text/html");
+        this.textArea.setHighlighter(null);
         this.textArea.setText(
                 """
-                Assignment
+                <style>
+                    code { font-size: 2rem; }
+                </style>
+                <h1>Variable Assignment</h1>
+                <code>var := 94;</code>
+                <p>
+                    Defining variables allows values to be reused throughout the program.
+                    <ul>
+                    <li>The variable must be defined before use.</li>
+                    <li>You may only define Integers (whole numbers)</li>
+                    <li>Variable names must only contains letters, numbers (not at the start) or underscores</li>
+                    <li>Variable names must not be a keyword: for, if, else, begin, end, to, downto, then, do</li>
+                    </ul>
+                </p>
                 
-                <variable-name> := <int-number>;
+                <h1>Built In Functions</h1>
                 
-                You can only define integer types.
+                <code>Position(x, y);</code>
+                <p>
+                A function sets the cursor to start drawing. This can be set at any point. Arguments Include:
+                <ul>
+                    <li><code>x</code> => The x position</li>
+                    <li><code>y</code> => The y position</li>
+                </ul>
+                </p>
                 
-                Functions
+                <code>Move(x, y);</code>
+                <p>
+                A function that move the cursor by specified x and y direction. Arguments Include:
+                <ul>
+                    <li><code>x</code> => Move in the x direction</li>
+                    <li><code>y</code> => Move in the y direction</li>
+                </ul>
+                </p>
                 
-                Position(<x>, <y>);
-                Move(<x>, <y>);
-                Rect(<x>, <y>, <width>, <height>;
+                <code>Rect(x, y, width, height);</code>
+                <p>
+                A function that draws a rectangle. Arguments Include:
+                <ul>
+                    <li><code>x</code> => Start x coordinate of the rectangle</li>
+                    <li><code>y</code> => Start y coordinate of the rectangle</li>
+                    <li><code>width</code> => Width of the rectangle</li>
+                    <li><code>height</code> => Height of the rectangle</li>
+                </ul>
+                </p>
                 
-                Loop
-                
-                For <variable-name> := <start> to | downto <end>
-                begin
-                    <statements>
-                end;
-                
-                If statements
-                
-                If <variable> <equality-symbol> <number> then
-                begin
-                end;
-                
-                There is no else statement available.
                 """);
 
         this.scrollPane.setViewportView(this.textArea);
